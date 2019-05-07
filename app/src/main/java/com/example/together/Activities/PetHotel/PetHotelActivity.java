332667ecab7d6f8e4a4c550ea440e0c48ece0e7d
dodose <45454801+dodose@users.noreply.github.com>
2019-05-07 18:18:45 +0900

    20190507 (Tuesday) 임시저장(맥으로 옮기기)

--- app/src/main/java/com/example/together/Activities/PetHotel/PetHotelActivity.java
+++ app/src/main/java/com/example/together/Activities/PetHotel/PetHotelActivity.java
@@ -2,6 +2,7 @@ package com.example.together.Activities.PetHotel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.together.R;
 
@@ -11,5 +12,10 @@ public class PetHotelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_hotel);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
