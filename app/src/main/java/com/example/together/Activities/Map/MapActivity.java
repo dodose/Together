6313cdfd70b8daa543986765c809065eae74c336
dodose <45454801+dodose@users.noreply.github.com>
2019-05-08 15:28:11 +0900

    학교컴 작업을 위한 커밋

---
+++ app/src/main/java/com/example/together/Activities/Map/MapActivity.java
@@ -0,0 +1,17 @@
+package com.example.together.Activities.Map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.together.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+    }
+}
