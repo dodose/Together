34997b6ab6e503ba64f2909c2b4d59803cdaf98f
dodose <45454801+dodose@users.noreply.github.com>
2019-05-16 09:59:32 +0900

    20190516 (목요일) 마이펫 리스트 아이템 전반적인 뷰 폼 수정

--- app/src/main/java/com/example/together/Adapter/PetAdapter.java
+++ app/src/main/java/com/example/together/Adapter/PetAdapter.java
@@ -1,4 +1,92 @@
 package com.example.together.Adapter;
 
-public class PetAdapter {
+import android.support.v7.widget.RecyclerView;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.together.Model.Pet;
+import com.example.together.R;
+
+import java.util.List;
+
+
+/**
+ * Created by Aws on 28/01/2018.import java.util.List;
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private Context mContext ;
+    private List<Pet> mData ;
+
+
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view ;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.item_mydoglist,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+        holder.tv_book_title.setText(mData.get(position).getTitle());
+        holder.img_book_thumbnail.setImageResource(mData.get(position).getThumbnail());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(mContext,Book_Activity.class);
+
+                // passing data to the book activity
+                intent.putExtra("Title",mData.get(position).getTitle());
+                intent.putExtra("Description",mData.get(position).getDescription());
+                intent.putExtra("Thumbnail",mData.get(position).getThumbnail());
+                // start the activity
+                mContext.startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_book_title;
+        ImageView img_book_thumbnail;
+        CardView cardView ;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            tv_book_title = (TextView) itemView.findViewById(R.id.book_title_id) ;
+            img_book_thumbnail = (ImageView) itemView.findViewById(R.id.book_img_id);
+            cardView = (CardView) itemView.findViewById(R.id.cardview_id);
+
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/together/Model/Pet.java
+++ app/src/main/java/com/example/together/Model/Pet.java
@@ -2,9 +2,66 @@ package com.example.together.Model;
 
 public class Pet {
     private String petName;
-    private int petAge;
     private String petBloodType;
+    private String description;
+    private int thumbnail;
     private int gender;
-    private String intro;
+    private int petAge;
 
+    public Pet(String petName, String petBloodType, String description, int thumbnail, int gender, int petAge) {
+        this.petName = petName;
+        this.petBloodType = petBloodType;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.gender = gender;
+        this.petAge = petAge;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getPetBloodType() {
+        return petBloodType;
+    }
+
+    public void setPetBloodType(String petBloodType) {
+        this.petBloodType = petBloodType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getPetAge() {
+        return petAge;
+    }
+
+    public void setPetAge(int petAge) {
+        this.petAge = petAge;
+    }
 }
