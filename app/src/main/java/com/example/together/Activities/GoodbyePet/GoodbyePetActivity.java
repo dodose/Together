a4aed5b3a21000012f224193f803c6fe0295ba23
dodose <45454801+dodose@users.noreply.github.com>
2019-06-04 12:10:48 +0900

    임시저

--- app/src/main/java/com/example/together/Activities/GoodbyePet/GoodbyePetActivity.java
+++ app/src/main/java/com/example/together/Activities/GoodbyePet/GoodbyePetActivity.java
@@ -1,15 +1,41 @@
 package com.example.together.Activities.GoodbyePet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.together.Activities.Map.MapActivity;
+import com.example.together.Activities.Map.MapsActivity;
 import com.example.together.R;
 
 public class GoodbyePetActivity extends AppCompatActivity {
+
+    ImageButton goodbyeMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goodbye_pet);
+
+        goodbyeMap = findViewById(R.id.goodbyeMap);
+
+
+
+
+        goodbyeMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GoodbyePetActivity.this, MapsActivity.class));
+            }
+        });
+
+
     }
+
+
+
+
+
 }
