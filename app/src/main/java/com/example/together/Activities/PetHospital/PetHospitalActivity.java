2248db5e311e4da8af53048ccab40001d001029d
llov0310 <46309820+llov0310@users.noreply.github.com>
2019-06-04 11:42:19 +0900

    0604 hotel commit

--- app/src/main/java/com/example/together/Activities/PetHospital/PetHospitalActivity.java
+++ app/src/main/java/com/example/together/Activities/PetHospital/PetHospitalActivity.java
@@ -1,38 +1,38 @@
-//package com.example.together.Activities.PetHospital;
-//
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//
-//import com.example.together.Activities.PetHotel.PetHotelActivity;
-//import com.example.together.R;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import android.support.v4.app.Fragment;
-//
-//public class PetHospitalActivity extends AppCompatActivity implements OnMapReadyCallback {
-//
-//    MapFragment fragment;
-//    GoogleMap map;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_pet_hospital);
-//
-//        //프래그먼트 객체 연결
+package com.example.together.Activities.PetHospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.together.Activities.PetHotel.PetHotelActivity;
+import com.example.together.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import android.support.v4.app.Fragment;
+
+public class PetHospitalActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    MapFragment fragment;
+    GoogleMap map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_hospital);
+
+        //프래그먼트 객체 연결
 //        fragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmet1);
-//
-//
-//
-//
-//
-//
-//    }
-//
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//
-//    }
-//}
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
