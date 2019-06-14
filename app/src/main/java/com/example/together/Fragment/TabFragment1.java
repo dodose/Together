1d8f2da7baf445b0b14b9c59d32bc6748c0f517a
dodose <45454801+dodose@users.noreply.github.com>
2019-06-14 21:55:09 +0900

    Fragment 추가

--- app/src/main/java/com/example/together/Fragment/TabFragment1.java
+++ app/src/main/java/com/example/together/Fragment/TabFragment1.java
@@ -1,21 +1,94 @@
 package com.example.together.Fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.example.together.R;
-import com.thoughtbot.expandablerecyclerview.ExpandableRecyclerViewAdapter;
 
 
-public class TabFragment1 extends Fragment  {
 
+public class TabFragment1 extends Fragment {
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+        View v = inflater.inflate(R.layout.tab_fragment_1, null);
+        ExpandableListView elv = (ExpandableListView) v.findViewById(R.id.elv_list);
+        elv.setAdapter(new SavedTabsListAdapter());
+        return v;
     }
+
+    public class SavedTabsListAdapter extends BaseExpandableListAdapter {
+
+        private String[] groups = { "People Names", "Dog Names", "Cat Names", "Fish Names" };
+
+        private String[][] children = {
+                { "Arnold", "Barry", "Chuck", "David" },
+                { "Ace", "Bandit", "Cha-Cha", "Deuce" },
+                { "Fluffy", "Snuggles" },
+                { "Goldy", "Bubbles" }
+        };
+
+        @Override
+        public int getGroupCount() {
+            return groups.length;
+        }
+
+        @Override
+        public int getChildrenCount(int i) {
+            return children[i].length;
+        }
+
+        @Override
+        public Object getGroup(int i) {
+            return groups[i];
+        }
+
+        @Override
+        public Object getChild(int i, int i1) {
+            return children[i][i1];
+        }
+
+        @Override
+        public long getGroupId(int i) {
+            return i;
+        }
+
+        @Override
+        public long getChildId(int i, int i1) {
+            return i1;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(TabFragment1.this.getActivity());
+            textView.setText(getGroup(i).toString());
+            return textView;
+        }
+
+        @Override
+        public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(TabFragment1.this.getActivity());
+            textView.setText(getChild(i, i1).toString());
+            return textView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return true;
+        }
+
+    }
+
+
 }
