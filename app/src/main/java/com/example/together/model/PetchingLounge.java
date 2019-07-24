61b71a50fe6e4ea7421bc749d2f09588f9badd6c
dodose <45454801+dodose@users.noreply.github.com>
2019-07-24 21:37:11 +0900

    Lounge 작업 Test Commit Requestor user 정보 가져와서 뿌리기

--- app/src/main/java/com/example/together/model/PetchingLounge.java
+++ app/src/main/java/com/example/together/model/PetchingLounge.java
@@ -1,4 +1,42 @@
 package com.example.together.model;
 
 public class PetchingLounge {
+
+    private String petBunyangId;
+    private String petFirendId;
+    private String petchingLounge;
+
+    public PetchingLounge(String petBunyangId, String petFirendId, String petchingLounge) {
+        this.petBunyangId = petBunyangId;
+        this.petFirendId = petFirendId;
+        this.petchingLounge = petchingLounge;
+    }
+
+    public PetchingLounge() {
+    }
+
+
+    public String getPetBunyangId() {
+        return petBunyangId;
+    }
+
+    public void setPetBunyangId(String petBunyangId) {
+        this.petBunyangId = petBunyangId;
+    }
+
+    public String getPetFirendId() {
+        return petFirendId;
+    }
+
+    public void setPetFirendId(String petFirendId) {
+        this.petFirendId = petFirendId;
+    }
+
+    public String getPetchingLounge() {
+        return petchingLounge;
+    }
+
+    public void setPetchingLounge(String petchingLounge) {
+        this.petchingLounge = petchingLounge;
+    }
 }
