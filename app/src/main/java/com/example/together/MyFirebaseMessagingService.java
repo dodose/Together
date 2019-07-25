6f5ea15697d70d39809fb30f9269df1c053f2de0
llov0310 <46309820+llov0310@users.noreply.github.com>
2019-07-25 09:50:39 +0900

    앱푸쉬및 채팅 푸쉬 완료 0725

---
+++ app/src/main/java/com/example/together/MyFirebaseMessagingService.java
@@ -0,0 +1,109 @@
+package com.example.together;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.together.activities.HomeActivity;
+import com.example.together.activities.chat.ChatsActivity;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG="FirebaseMessageService";
+
+   String bodyMessage,title;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        // TODO(developer): Handle FCM messages here.
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            bodyMessage = remoteMessage.getNotification().getBody();
+
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification title: " + remoteMessage.getNotification().getTitle());
+            title = remoteMessage.getNotification().getTitle();
+        }
+
+        String act = "HomeActivity";
+
+        sendNotification(bodyMessage,title);
+
+    }
+
+    private void sendNotification(String messageBody,String title) {
+
+        Intent intent = new Intent(this, ChatsActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "default";
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder =
+
+                new NotificationCompat.Builder(this, channelId)
+
+                        .setSmallIcon(R.mipmap.ic_launcher)
+
+                        .setContentTitle(title)
+
+                        .setContentText(messageBody)
+
+                        .setAutoCancel(true)
+
+                        .setSound(defaultSoundUri)
+
+                        .setContentIntent(pendingIntent);
+
+
+
+        NotificationManager notificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String channelName = "default";
+
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+    }
